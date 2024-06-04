f557840ac2a31ef6898daddc4e499c65b4be8ef6
gracie-123 <gracietumbo@gmail.com>
2024-06-04 10:08:59 +0000

    0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,10 @@
 export default function cleanSet(set, startString) {
-  let result = '';
-
-  set.forEach((value) => {
-    const stringValue = String(value);
-    if (stringValue.startsWith(startString)) {
-      const restOfString = stringValue.slice(startString.length);
-      if (result.length > 0) {
-        result += '-';
-      }
-      result += restOfString;
+  let res = '';
+  if (!startString || !startString.length) return res;
+  for (const el of set) {
+    if (el && el.startsWith(startString)) {
+      res += `${el.slice(startString.length)}-`;
     }
-  });
-
-  return result;
+  }
+  return res.slice(0, res.length - 1);
 }
